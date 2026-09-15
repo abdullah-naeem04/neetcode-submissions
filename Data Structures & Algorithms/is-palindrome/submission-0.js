9ac329835275736ed26b0877c0d376f6e4e8162b
Abdullah Naeem <122016061+abdullah-naeem04@users.noreply.github.com>
2026-09-15 13:42:44 +0500

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let trimedString = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "")
+        console.log(trimedString)        
+        let left = 0;
+        let right = trimedString.length - 1;
+        while (left < right){
+            if(trimedString[left] === trimedString[right]){
+                left ++
+                right --
+            }
+            else{
+                return false
+
+            }
+        }
+            return true
+    }
+}
